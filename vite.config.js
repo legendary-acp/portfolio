1acc73754c5f4701cf29c42b407b86a58b69d1b6
Pradyuman <meetupradyuman@gmail.com>
2024-09-07 13:50:21 +0530

    testing changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/'
-})
+  plugins: [react(), vercel()],
+  base: "/portfolio/",
+});
